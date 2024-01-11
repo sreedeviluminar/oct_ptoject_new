51fdecf5a40110869b31a72b96d9006aa4f30021
sreedeviluminar <sreedevi@luminartechnolab.com>
2024-01-11 14:36:56 +0530

    initial commit

---
+++ lib/sliversEx.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: SliverEx(),
+  ));
+}
+
+class SliverEx extends StatelessWidget {
+  const SliverEx({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: CustomScrollView(
+              slivers: [
+              SliverAppBar(
+              floating: true,
+              pinned: true,
+              title: const Text("SliverExample"),
+              bottom: AppBar(
+                title: Container(
+                  width: double.infinity,
+                  height: 40,
+                  color: Colors.white,
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search Here",
+                      prefixIcon: Icon(Icons.search),
+                      suffixIcon: Icon(Icons.camera_alt),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                      (context, index) =>
+                      Card(
+                        child: ListTile(
+                          leading: Image.asset("assets/images/icon.png"),
+                          title: Text("Product"),
+                          trailing: Icon(Icons.shopping_cart),
+                        ),
+                      ),
+                  childCount: 10),
+            ),
+                SliverToBoxAdapter(
+                    child: Container(
+                      height: 200,
+                      color: Colors.black,
+                      child: GridView.builder(
+                        itemCount: 5
+                        ,
+                        scrollDirection: Axis.horizontal,
+                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 1),
+                        itemBuilder: (context, index) {
+                          return Card(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Expanded(child: Image.asset("assets/images/logingirl.png")),
+                                const Text(
+                                  "Login Girl",
+                                  style: TextStyle(
+                                      fontSize: 25, fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                ),
+            SliverGrid(
+                delegate: SliverChildBuilderDelegate((context, index) =>
+                    Card(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Image.asset("assets/images/mango.png"),
+                          const Text(
+                            "Mango",
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold),
+                          ),
+                          const Text("\$200/kg",
+                              style: TextStyle(
+                                fontSize: 10,
+                              ))
+                        ],
+                      ),
+                    ),
+                    childCount: 10
+                ),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisSpacing: 15,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 2)),
+
+
+              ],
+            )));
+  }
+}
