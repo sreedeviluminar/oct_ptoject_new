f2e1d034798f8f45b828513b98407423c88ee8ab
sreedeviluminar <sreedevi@luminartechnolab.com>
2024-03-01 12:02:23 +0530

    initial commit

--- lib/api integration/using dio/screens/diomain.dart
+++ lib/api integration/using dio/screens/diomain.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:lottie/lottie.dart';
 import 'package:oct_ptoject_new/api%20integration/using%20dio/controller/datacontroller.dart';
 import 'package:oct_ptoject_new/api%20integration/using%20dio/utils/colors.dart';
+import 'package:oct_ptoject_new/api%20integration/using%20dio/utils/const_snackbar.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 void main() {
   runApp(GetMaterialApp(home: DioHome()));
@@ -28,9 +31,8 @@ class DioHome extends StatelessWidget {
             height: double.infinity,
             child: controller.isNetConnected.value
                 ? (controller.isLoading.value)
-                ? Center(child: Lottie.asset(
-                "assets/animation/Animation - 1709191722675.json")) : getData()
-                : noInternet(),
+                ? Center(child: Lottie.asset("assets/animation/Animation - 1709191722675.json")) : getData()
+                : noInternet(context),
           )),
     );
   }
@@ -48,7 +50,42 @@ class DioHome extends StatelessWidget {
     );
   }
 
-  getData() {}
+  RefreshIndicator getData() async {
+    return RefreshIndicator(
+        child: ScrollablePositionedList.builder(
+            itemCount: controller.datas.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: MyColors.bgColor,
+                    child: Text(controller.datas[index].id.toString()),
+                  ),
+                  title: Text(controller.datas[index].title!),
+                  subtitle: Text(controller.datas[index].body!),
+                ),
+              );
+            }),
+        onRefresh: () {
+          return controller.fetchData();
+        });
+  }
 
-  noInternet() {}
+  Center noInternet(BuildContext context) {
+    return Center(
+      child:  Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Lottie.asset("assets/animation/networkerror.json"),
+          MaterialButton(onPressed: ()async{
+            if(await InternetConnectionChecker().hasConnection){
+              controller.fetchData();
+            }else{
+              showCustomSnackBar(context);
+            }
+          })
+        ],
+      ),
+    );
+  }
 }
